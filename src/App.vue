3bc703e072ece9471aa51860732cdf0ae6e85fd3
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-12-01 15:10:03 +0100

    Functional Game on Vue 3

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<!-- @format -->
+
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p v-if="showResult">Reaction Time: {{ score }} ms</p>
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+export default {
+  name: "App",
+  components: {
+    Block,
+  },
+
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false,
+    };
+  },
+
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResult = false;
+      // console.log(this.delay);
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
